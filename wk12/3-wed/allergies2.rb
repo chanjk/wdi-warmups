efe290c188caeb0ad94c0fea0a76462a1d5549ad
Julian Chan <julian.chan@mail.com>
2017-04-26 10:35:22 +1000

    added another file with another solution using & and select instead

---
+++ wk12/3-wed/allergies2.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Allergies
+  @@all_allergies = %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats)
+                      .map.with_index { |a, i| [a, 2 ** i] }.to_h
+
+  def initialize(score)
+    @score = score
+    @allergies = @@all_allergies.select { |a, v| score & v > 0 }.keys
+  end
+
+  def list
+    @allergies
+  end
+
+  def allergic_to?(item)
+    @allergies.include?(item)
+  end
+end
+
+allergies = Allergies.new(34)
+puts allergies.allergic_to?('chocolate')
+puts allergies.allergic_to?('cats')
+puts allergies.list
+
+binding.pry
